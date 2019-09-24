91608f0ccf4b377a9ac4dd569e85552a09506e19
Adam Butterworth <abutterworth@edx.org>
2019-09-24 16:32:51 -0400

    Update webpack.dev.config.js

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -136,7 +136,7 @@ module.exports = Merge.smart(commonConfig, {
   // reloading.
   devServer: {
     host: '0.0.0.0',
-    port: process.env.PORT,
+    port: process.env.PORT || 8080,
     historyApiFallback: true,
     hot: true,
     inline: true,
