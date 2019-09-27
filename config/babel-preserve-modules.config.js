87017b42ef1c67f5103b61aa2e1670fdb9fab9dc
Adam Butterworth <abutterworth@edx.org>
2019-09-27 10:22:47 -0400

    feat: add a babel-preserve-modules config preset (#11)

---
+++ config/babel-preserve-modules.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+  ],
+  env: {
+    i18n: {
+      plugins: [
+        [
+          'react-intl',
+          {
+            messagesDir: './temp/babel-plugin-react-intl',
+            moduleSourceName: '@edx/frontend-i18n',
+          },
+        ],
+      ],
+    },
+    test: {
+      presets: [
+        '@babel/preset-env',
+      ],
+    },
+  },
+};
