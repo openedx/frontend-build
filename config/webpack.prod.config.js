c7d57b0bc71214eb0fa15bda48b70602e6881671
David Joy <djoy@edx.org>
2019-12-02 11:24:03 -0500

    build: removing new-relic-source-map-webpack-plugin
    Want to see if this fixes the profile build.

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -7,7 +7,6 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackNewRelicPlugin = require('html-webpack-new-relic-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const NewRelicSourceMapPlugin = require('new-relic-source-map-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const PostCssRtlPlugin = require('postcss-rtl');
 const PostCssAutoprefixerPlugin = require('autoprefixer');
@@ -164,12 +163,6 @@ module.exports = Merge.smart(commonConfig, {
       // We use non empty strings as defaults here to prevent errors for empty configs
       license: process.env.NEW_RELIC_LICENSE_KEY || 'fake_app',
       applicationID: process.env.NEW_RELIC_APP_ID || 'fake_license',
-    }),
-    new NewRelicSourceMapPlugin({
-      applicationId: process.env.NEW_RELIC_APP_ID,
-      nrAdminKey: process.env.NEW_RELIC_ADMIN_KEY,
-      staticAssetUrl: process.env.BASE_URL,
-      noop: typeof process.env.NEW_RELIC_ADMIN_KEY === 'undefined', // upload source maps in prod builds only
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
