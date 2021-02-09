259a13bf8e215ed080fc7cc60aba4b01df8ce884
David Joy <djoy@edx.org>
2021-02-09 14:55:50 -0500

    fix: bumping to webpack-dev-server 4.0.0 beta
    The 3.x version of the dev server is not compatible with webpack 5 or the latest webpack-cli.  Everything explodes.  Also bumping react-dev-utils to get the latest verison of the webpackHotDevClient.

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -230,16 +230,18 @@ module.exports = merge(commonConfig, {
     historyApiFallback: {
       index: path.join(PUBLIC_PATH, 'index.html'),
     },
-    // Enable hot reloading server. It will provide WDS_SOCKET_PATH endpoint
-    // for the WebpackDevServer client so it can learn when the files were
-    // updated. The WebpackDevServer client is included as an entry point
-    // in the webpack development configuration. Note that only changes
-    // to CSS are currently hot reloaded. JS changes will refresh the browser.
-    hot: true,
-    // Use 'ws' instead of 'sockjs-node' on server since we're using native
-    // websockets in `webpackHotDevClient`.
-    transportMode: 'ws',
-    inline: true,
-    publicPath: PUBLIC_PATH,
+
+    dev: {      // Enable hot reloading server. It will provide WDS_SOCKET_PATH endpoint
+      // for the WebpackDevServer client so it can learn when the files were
+      // updated. The WebpackDevServer client is included as an entry point
+      // in the webpack development configuration. Note that only changes
+      // to CSS are currently hot reloaded. JS changes will refresh the browser.
+      hot: true,
+      // Use 'ws' instead of 'sockjs-node' on server since we're using native
+      // websockets in `webpackHotDevClient`.
+      transportMode: 'ws',
+      inline: true,
+      publicPath: PUBLIC_PATH,
+    },
   },
 });
