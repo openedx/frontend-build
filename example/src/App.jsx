76abdb3c92713564f22db6bba21813d1bebffbc6
David Joy <djoy@edx.org>
2021-06-17 12:37:21 -0400

    test: adding RTL-sensitive CSS so we can test it

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -22,6 +22,7 @@ export default function App() {
       <p>Photo by Louis Hansel @shotsoflouis on Unsplash</p>
       <p>Test variable: {process.env.TEST_VARIABLE}</p>
       <p>Non-existent variable: {process.env.I_AM_NOT_HERE}</p>
+      <p className="text-align-right">I&apos;m aligned right, but left in RTL.</p>
     </div>
   );
 }
--- example/src/index.jsx
+++ example/src/index.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// This line is to emulate what frontend-platform does when i18n initializes.
+// It's necessary because our stylesheet is generated with `[dir="ltr"]` as a prefix on all
+// direction-sensitive CSS classes.  Without this line, those classes wouldn't be applied to the
+// document.  See: https://github.com/edx/frontend-platform/blob/master/src/i18n/lib.js#L186
+global.document.getElementsByTagName('html')[0].setAttribute('dir', 'ltr');
+
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
