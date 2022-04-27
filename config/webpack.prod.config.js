539faa9d8b1b04c49de2303f241b041c7e378721
Alex Dusenbery <adusenbery@edx.org>
2022-04-27 16:36:05 -0400

    fix: add NODE_ENV variable to dev html webpack plugin

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -189,6 +189,7 @@ module.exports = merge(commonConfig, {
       template: path.resolve(process.cwd(), 'public/index.html'),
       FAVICON_URL: process.env.FAVICON_URL || null,
       OPTIMIZELY_PROJECT_ID: process.env.OPTIMIZELY_PROJECT_ID || null,
+      NODE_ENV: process.env.NODE_ENV || null,
     }),
     new Dotenv({
       path: path.resolve(process.cwd(), '.env'),
