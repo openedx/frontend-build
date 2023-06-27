cd2097827d24029805321ad5233024d54086a609
Adam Stankiewicz <agstanki@gmail.com>
2023-06-27 08:00:30 -0400

    feat: add serve command to fedx-scripts to run production bundle

--- example/env.config.js
+++ example/env.config.js
@@ -3,4 +3,5 @@ module.exports = {
   CORRECT_BOOL_VALUE: 'Good, false meant false.  We did not cast a boolean to a string.',
   INCORRECT_BOOL_VALUE: 'Why was a false boolean true?',
   INTEGER_VALUE: 123,
+  PORT: 3000,
 };
