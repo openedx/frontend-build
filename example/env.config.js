f4c9e86baa741034fbfb5e50cf4f68872206d530
Adam Stankiewicz <agstanki@gmail.com>
2023-07-17 17:23:52 -0400

    feat: support .env.development and .env.private

--- example/env.config.js
+++ example/env.config.js
@@ -3,5 +3,4 @@ module.exports = {
   CORRECT_BOOL_VALUE: 'Good, false meant false.  We did not cast a boolean to a string.',
   INCORRECT_BOOL_VALUE: 'Why was a false boolean true?',
   INTEGER_VALUE: 123,
-  PORT: 3000,
 };
