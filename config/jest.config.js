7b5639d60ced57857d9805dbe31519f5dea04fb3
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2023-12-27 18:42:27 +0500

    fix: fixed jest config to ignore @opendedx namespaced packages

--- config/jest.config.js
+++ config/jest.config.js
@@ -30,7 +30,7 @@ module.exports = {
     'setupTest.js',
   ],
   transformIgnorePatterns: [
-    '/node_modules/(?!@edx)',
+    '/node_modules/(?!@(open)?edx)',
   ],
   transform: {
     '^.+\\.[t|j]sx?$': [
