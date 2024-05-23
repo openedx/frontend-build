2bf31f8a4773702bb3573450f714dd0dd6b4a2c1
Diana Catalina Olarte <diana.olarte@edunext.co>
2024-05-23 17:09:06 +1000

    revert: example changes

--- example/env.config.js
+++ example/env.config.js
@@ -3,29 +3,4 @@ module.exports = {
   CORRECT_BOOL_VALUE: 'Good, false meant false.  We did not cast a boolean to a string.',
   INCORRECT_BOOL_VALUE: 'Why was a false boolean true?',
   INTEGER_VALUE: 123,
-  PARAGON_THEME_URLS: {
-    core: {
-      urls: {
-        default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@alpha/dist/core.min.css',
-        brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@alpha/dist/core.min.css',
-      },
-    },
-    defaults: {
-      light: 'light',
-    },
-    variants: {
-      light: {
-        urls: {
-          default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@alpha/dist/light.min.css',
-          brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@alpha/dist/light.min.css',
-        },
-      },
-      dark: {
-        urls: {
-          default: 'https://cdn.jsdelivr.net/npm/@openedx/paragon@v21.0.0-alpha.40/dist/light.min.css',
-          brandOverride: 'https://cdn.jsdelivr.net/npm/@edx/brand-edx.org@2.2.0-alpha.13/dist/light.min.css',
-        },
-      },
-    },
-  },
 };
--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,11 +1,4 @@
 import ReactDOM from 'react-dom';
-import {
-  AppProvider,
-  ErrorPage,
-} from '@edx/frontend-platform/react';
-import { APP_INIT_ERROR, APP_READY, initialize } from '@edx/frontend-platform';
-import { subscribe } from '@edx/frontend-platform/pubSub';
-
 import App from './App';
 
 // This line is to emulate what frontend-platform does when i18n initializes.
@@ -14,21 +7,7 @@ import App from './App';
 // document.  See: https://github.com/openedx/frontend-platform/blob/master/src/i18n/lib.js#L186
 global.document.getElementsByTagName('html')[0].setAttribute('dir', 'ltr');
 
-subscribe(APP_READY, () => {
-  ReactDOM.render(
-    <AppProvider>
-      <App />
-    </AppProvider>,
-    document.getElementById('root'),
-  );
-});
-
-subscribe(APP_INIT_ERROR, (error) => {
-  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
-});
-
-initialize({
-  messages: [],
-  requireAuthenticatedUser: false,
-  hydrateAuthenticatedUser: true,
-});
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
