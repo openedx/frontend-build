f27220c904e18307083a0317dd5e016dabf0b07b
Diana Catalina Olarte <diana.olarte@edunext.co>
2024-05-28 20:47:42 +1000

    feat: add example changes for test ParagonPlugin

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -1,4 +1,11 @@
 import ReactDOM from 'react-dom';
+import {
+  AppProvider,
+  ErrorPage,
+} from '@edx/frontend-platform/react';
+import { APP_INIT_ERROR, APP_READY, initialize } from '@edx/frontend-platform';
+import { subscribe } from '@edx/frontend-platform/pubSub';
+
 import App from './App';
 
 // This line is to emulate what frontend-platform does when i18n initializes.
@@ -7,7 +14,21 @@ import App from './App';
 // document.  See: https://github.com/openedx/frontend-platform/blob/master/src/i18n/lib.js#L186
 global.document.getElementsByTagName('html')[0].setAttribute('dir', 'ltr');
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
-);
+subscribe(APP_READY, () => {
+  ReactDOM.render(
+    <AppProvider>
+      <App />
+    </AppProvider>,
+    document.getElementById('root'),
+  );
+});
+
+subscribe(APP_INIT_ERROR, (error) => {
+  ReactDOM.render(<ErrorPage message={error.message} />, document.getElementById('root'));
+});
+
+initialize({
+  messages: [],
+  requireAuthenticatedUser: false,
+  hydrateAuthenticatedUser: true,
+});
